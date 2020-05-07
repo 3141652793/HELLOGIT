569dfa806608cedf082626c4b383edbc717b460f
876202761 <876202761@qq.com>
2020-05-07 14:17:25 +0800

    876202761 产生冲突src/main/java/org/example/App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,7 +5,8 @@ package org.example;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World1!");
         System.out.println("Hello Git");
+        System.out.println("我后提交");
     }
 }
